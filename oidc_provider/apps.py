b038ccd2bb6825a8abdfd4ade28da071c78331bf
ashraf-springpeople <98261846+ashraf-springpeople@users.noreply.github.com>
2022-02-17 16:53:19 +0530

    Update apps.py

--- oidc_provider/apps.py
+++ oidc_provider/apps.py
@@ -5,3 +5,11 @@ class OIDCProviderConfig(AppConfig):
 
     name = 'oidc_provider'
     verbose_name = u'OpenID Connect Provider'
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': 'oidc_provider',
+                'regex': r'^openid/',
+                'relative_path': 'oidc_provider.urls',
+            }
+        }
